acd758544552cec2d6a446db61b7821129800987
Kleberson Santos <kleberson2santos@gmail.com>
2021-04-04 15:22:28 -0400

    lista de livros

--- src/main/java/br/com/kleberson2santos/casadocodigov1/cadastrolivro/LivrosController.java
+++ src/main/java/br/com/kleberson2santos/casadocodigov1/cadastrolivro/LivrosController.java
@@ -1,24 +1,30 @@
 package br.com.kleberson2santos.casadocodigov1.cadastrolivro;
 
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
+import java.util.List;
 
+@RequestMapping("/livros")
 @RestController
 public class LivrosController {
     @PersistenceContext
     private EntityManager manager;
 
-    @PostMapping("/livros")
+    @PostMapping
     @Transactional
     public String cria(@RequestBody @Valid NovoLivroRequest request) {
         Livro novoLivro = request.toModel(manager);
         manager.persist(novoLivro);
         return novoLivro.toString();
+    }
+
+    @GetMapping
+    public List lista() {
+        //TODO: criar um Response
+        return manager.createQuery("from Livro").getResultList();
     }
 }
